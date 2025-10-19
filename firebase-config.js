2b733ce218655a4cd11df682bb0b8d2b519eaac7
Tibutec <contacto.tibutec@gmail.com>
2025-10-19 18:43:27 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+// firebase-config.js
+// Configuración compartida de Firebase
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_eIHjEUx9vPt_6FMmuTMEr9A2yNnv1RE",
+    authDomain: "tibutec-website.firebaseapp.com",
+    projectId: "tibutec-website",
+    storageBucket: "tibutec-website.firebasestorage.app",
+    messagingSenderId: "321326552697",
+    appId: "1:321326552697:web:0b0e1b7203efeb7444a6a8"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app, firebaseConfig };
